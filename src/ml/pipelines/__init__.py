e4e09ff80e3d99cbfc0171a39b4e4a4f4280035b
SLA2009 <sebastien.lamort@outlook.com>
2026-06-28 09:04:04 +0200

    Pipelines renaming

--- src/ml/pipelines/__init__.py
+++ src/ml/pipelines/__init__.py
@@ -1,5 +1,15 @@
 """Package de pipelines pour MLops."""
 
-from .inference_pipeline import InferencePipeline
+from .inference import InferencePipeline
+from .ingestion import IngestionPipeline
+from .monitoring import MonitoringPipeline
+from .preparation import PreparationPipeline
+from .training import TrainingPipeline
 
-__all__ = ["InferencePipeline"]
+__all__ = [
+    "InferencePipeline",
+    "IngestionPipeline",
+    "MonitoringPipeline",
+    "PreparationPipeline",
+    "TrainingPipeline"
+]
